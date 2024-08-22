776411c81e91635b9b42b29283e42f7ed17d1cc2
JuYoung <jjystar3@gmail.com>
2024-08-22 14:04:28 +0900

    숫자 함수 퀴즈 수정

--- 04_내장 객체/연습문제/01_Quiz.js
+++ 04_내장 객체/연습문제/01_Quiz.js
@@ -1,31 +1,28 @@
 // Q1
 let email = "user@naver.com";
-let str = email.split("@")
-console.log("도메인: " + str[1]);
+let domain = email.split("@")[1]
+console.log("도메인: " + domain);
 
 // Q2
-let badWords = "나쁜, 더러운";
-let badWord = badWords.split(", ");
-
+const badWords = ["나쁜", "더러운"];
 let message = "저 친구는 나쁜 친구야. 정말 더러운 사람 같아";
 
-for(let word of badWord){
-  if(message.indexOf(word) != -1){
-    let str = "";
-    for(let i in word){
-      str+="X";
-    }
-    message = message.replace(word, str);
-  }
+for(let word of badWords){
+  message = message.replaceAll(word, "X".repeat(word.length));
 }
 console.log(message);
 
 // Q3
+const allowedExtensions = ['.jpg', '.png', '.gif'];
 let file = "사과.txt";
-if(file.endsWith(".jpg") || file.endsWith(".png") || file.endsWith(".gif")){
-  console.log("지원되는 파일 형식입니다")
+
+const index = file.lastIndexOf(".");
+const extension = file.slice(index);
+
+if(allowedExtensions.includes(extension)){
+  console.log("지원되는 파일 형식입니다");
 }else{
-  console.log("지원되지 않는 파일 형식입니다")
+  console.log("지원되지 않는 파일 형식입니다");
 }
 
 // Q4
@@ -40,29 +37,27 @@ console.log(sum);
 let arr2 = [1,"aa",true,"5",10];
 let sum2 = 0;
 for (let val of arr2){
-  if(!isNaN(parseInt(val))){
-    sum2 += parseInt(val);
+  if(!isNaN(val)){
+    if(typeof val == "number" || typeof val == "boolean"){
+      sum2 += val;
+    }else if(typeof val == "string"){
+      sum2 += parseInt(val);
+    }
   }
 }
 console.log(sum2);
 
 // Q6
 let str3 = 'aaabbc';
-
-let checkstr = str3[0];
-let count = 1;
 let finalStr = "";
+let count = 1;
 
-for (let i=1;i<str3.length;i++){
-  if(str3[i] == checkstr){
+for (let i=0;i<str3.length;i++){
+  if(str3[i] === str3[i+1]){
     count++;
   }else{
-    finalStr += checkstr.concat(count);
-    checkstr = str3[i];
+    finalStr += str3[i].concat(count);
     count=1;
-  }
-  if(i == str3.length-1){
-    finalStr += checkstr.concat(count);
   }
 }
 console.log(finalStr);
