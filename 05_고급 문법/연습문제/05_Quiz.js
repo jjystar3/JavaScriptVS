f953d817f2237a51d7f87ec66cc1eb603606ec5a
JuYoung <jjystar3@gmail.com>
2024-08-29 09:43:38 +0900

    퀴즈 수정

--- 05_고급 문법/연습문제/05_Quiz.js
+++ 05_고급 문법/연습문제/05_Quiz.js
@@ -40,6 +40,18 @@ result = str.match(patt);
 console.log(result);
 
 // Q36
+let str8 = "2024-08-27";
+//\d: 숫자
+//{숫자}: 앞에 패턴이 지정된 횟수만큼 반복됨
+//(): 캡처 그룹으로, 일치하는 부분만 문자열로 묶어서 추출
+//(\d{4}): 첫번째 그룹 (연도)
+//(\d{2}): 두번째 그룹 (월)
+//(\d{2}): 셋번째 그룹 (일)
+let dateParts = str8.match(/(\d{4})-(\d{2})-(\d{2})/); //숫자4개-숫자2개-숫자2개
+console.log(dateParts); 
+
+console.log(dateParts[3]); 
+
 str = "2024-08-27";
 patt = /-/;
 result = str.split(patt);
