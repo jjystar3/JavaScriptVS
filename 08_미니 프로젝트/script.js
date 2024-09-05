0ce537e6ada88fbc70c1ed56c93ba75891f6e971
JuYoung <jjystar3@gmail.com>
2024-09-05 12:38:02 +0900

    javascript optimized

--- 08_미니 프로젝트/script.js
+++ 08_미니 프로젝트/script.js
@@ -1,77 +1,60 @@
-let listArr = [];
 const todoList = document.getElementById('addedList');
+const todoCounts =  document.getElementById('status');
 
-let savedList = JSON.parse(localStorage.getItem("saved-list"));
+document.addEventListener('DOMContentLoaded', loadTodos());
 
-if (savedList != null) {
-  for (let i = 0; i < savedList.length; i++) {
-    listArr.push(savedList[i]);
-    addList(savedList[i]);
-  }
-}
-
-function addList(preList) {
+function addList() {
   let listText = document.getElementById("listTxt");
-  let listTextValue = null;
-
-  if (preList === undefined) {
-    listTextValue = { checked: false, value: listText.value };
-    if(listText.value != ""){
-      listArr.push(listTextValue);
-    }
-    localStorage.setItem('saved-list', JSON.stringify(listArr));
-  } else {
-    listTextValue = { checked: preList.checked, value: preList.value };
+  
+  if(listText.value != "") {
+    createTodoElement(listText.value, false);    
+    saveTodos();
+    listText.value = "";
   }
+};
 
-  if (listTextValue.value != "") {
+function createTodoElement(text, isChecked){
 
-    let list = document.createElement('div');
-    list.classList.add("list");
-    todoList.appendChild(list);
+  let list = document.createElement('div');
+  list.classList.add("list");
+  todoList.appendChild(list);
 
-    let checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-    checkbox.classList.add("chkBox");
-    checkbox.setAttribute("onclick", "toggleFinished(this.parentElement)");
-    checkbox.checked = listTextValue.checked;
-    list.appendChild(checkbox);
+  let checkbox = document.createElement('input');
+  checkbox.type = "checkbox";
+  checkbox.classList.add("chkBox");
+  checkbox.setAttribute("onclick", "toggleFinished(this.parentElement)");
+  checkbox.checked = isChecked;
+  list.appendChild(checkbox);
 
-    let textlist = document.createElement('input');
-    textlist.type = "text";
-    textlist.classList.add("txtList");
-    textlist.classList.add("txtReadOnly");
-    textlist.value = listTextValue.value;
-    textlist.readOnly = true;
-    list.appendChild(textlist);
+  let textlist = document.createElement('input');
+  textlist.type = "text";
+  textlist.classList.add("txtList");
+  textlist.classList.add("txtReadOnly");
+  textlist.value = text;
+  textlist.readOnly = true;
+  list.appendChild(textlist);
 
-    let btn1 = document.createElement('input');
-    btn1.type = "button";
-    btn1.classList.add("btnList");
-    btn1.value = "수정";
-    btn1.setAttribute("onclick", "editList(this.parentElement)");
-    list.appendChild(btn1);
+  let btn1 = document.createElement('input');
+  btn1.type = "button";
+  btn1.classList.add("btnList");
+  btn1.value = "수정";
+  btn1.setAttribute("onclick", "editList(this.parentElement)");
+  list.appendChild(btn1);
 
-    let btn2 = document.createElement('input');
-    btn2.type = "button";
-    btn2.classList.add("btnList");
-    btn2.value = "삭제";
-    btn2.setAttribute("onclick", "deleteList(this.parentElement)");
-    list.appendChild(btn2);
+  let btn2 = document.createElement('input');
+  btn2.type = "button";
+  btn2.classList.add("btnList");
+  btn2.value = "삭제";
+  btn2.setAttribute("onclick", "deleteList(this.parentElement)");
+  list.appendChild(btn2);
 
-    if(listTextValue.checked){
-      list.classList.add("crossline");
-    }
-
+  if(isChecked){
+    list.classList.add("crossline");
   }
-
-  listText.value = "";
-  countStatus();
 };
 
 function toggleFinished(obj) {
   let checkBox = obj.querySelector('.chkBox');
-  let text1 = obj.querySelector('.txtList');
 
   if (checkBox.checked) {
     obj.classList.add("crossline");
@@ -79,65 +62,60 @@ function toggleFinished(obj) {
     obj.classList.remove("crossline");
   }
   
-  objIndex = listArr.findIndex(obj => obj.value == text1.value);
-  if (objIndex > -1) {
-    listArr[objIndex].checked = checkBox.checked;
-  }
-  localStorage.setItem('saved-list', JSON.stringify(listArr));
-  countStatus();
+  saveTodos();
 };
-
-let editText = "";
 
 function editList(obj) {
   let text1 = obj.querySelector('.txtList');
-  let btn = obj.querySelectorAll('.btnList');
+  let btn = obj.querySelectorAll('.btnList')[0];
 
-  if (btn[0].value == "수정") {
-    text1.classList.remove("txtReadOnly", "crossline");
+  if (btn.value == "수정") {
+    text1.classList.remove("txtReadOnly");
     text1.readOnly = false;
-    editText = text1.value;
-    btn[0].value = "저장";
-  } else if (btn[0].value == "저장") {
-    objIndex = listArr.findIndex(obj => obj.value == editText);
-    if (objIndex > -1) {
-      listArr[objIndex].value = text1.value;
-      if(listArr[objIndex].checked){
-        text1.classList.add("txtReadOnly", "crossline");
-      }else{
-        text1.classList.add("txtReadOnly");
-      }
-    }
-    localStorage.setItem('saved-list', JSON.stringify(listArr));  
+    btn.value = "저장";
+  } else if (btn.value == "저장") {    
+    saveTodos();
+    text1.classList.add("txtReadOnly");
     text1.readOnly = true;
-    btn[0].value = "수정";
-    editText = "";
+    btn.value = "수정";
   }
 }
 
 function deleteList(obj) {
-  let text1 = obj.querySelector('.txtList');
   obj.remove();
-  objIndex = listArr.findIndex(obj => obj.value == text1.value);
-  if (objIndex > -1) {
-    listArr.splice(objIndex, 1);
-  }
-  localStorage.setItem('saved-list', JSON.stringify(listArr));
-  countStatus();
+  saveTodos();
 }
 
 function deleteAllList() {
-  while (todoList.firstChild) {
-    todoList.removeChild(todoList.firstChild);
-  }
-  listArr = [];
-  localStorage.setItem('saved-list', JSON.stringify(listArr));
+  todoList.innerHTML = '';
+  saveTodos();
+}
+
+function saveTodos() {
+  const todos = [];  
+  todoList.querySelectorAll('div').forEach(listItem => {    
+    const todoText = listItem.querySelector('input[type="text"]').value;
+    const isCompleted = listItem.querySelector('input[type="checkbox"]').checked;
+    
+    todos.push({ checked: isCompleted, value: todoText });
+  });
+  
+  localStorage.setItem('saved-list', JSON.stringify(todos));
+
+  countStatus();
+}
+
+function loadTodos() {
+  let savedList = JSON.parse(localStorage.getItem("saved-list"));
+
+  savedList.forEach(todo => {
+      createTodoElement(todo.value, todo.checked);
+  });
+
   countStatus();
 }
 
 function countStatus() {
-  let todoCounts =  document.getElementById('status');
-
   let totalCount = todoList.children.length;
   let checkedCount =  todoList.querySelectorAll('input[type="checkbox"]:checked').length;
   todoCounts.textContent = `총 ${totalCount}개 완료 ${checkedCount}개`;
